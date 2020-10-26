92530f8ed8ee9d9767688a38eb1dbb9c75ce493e
Lucia <luciasanchezfraile@gmail.com>
2020-10-26 19:15:32 +0100

    test

--- notes.php
+++ notes.php
@@ -11,6 +11,7 @@ Domain Path:  /languages
 Text Domain:  notes
 */
 
+
 if( ! defined( 'ABSPATH' ) ) exit;
 
 /**
@@ -27,13 +28,13 @@ class Note {
     private $slug               = 'notes';
     private $name               = 'Notes';
     private $singular_name      = 'Note';
-   
-   
+
+
     public function __construct() {
         // Register the post type
         add_action('init', array($this, 'registerCPT'));
     }
-    
+
      /**
      * Register post type
      */
@@ -57,7 +58,7 @@ class Note {
             'labels'                => $labels,
             'public'                => false, // the CPT will not have a page on the front-end
             'publicly_queryable'    => false, // the content of the notes will not be found on search
-            'show_ui'               => false, // notes are not editable. 
+            'show_ui'               => false, // notes are not editable.
             'show_in_menu'          => false, //the CPT will not have its own page on the admin
             'query_var'             => true,
             'rewrite'               => array( 'slug' => $this->slug ),
@@ -70,13 +71,13 @@ class Note {
         );
         register_post_type( $this->type, $args );
     }
-      
+
 }
 
 /**
  * Instantiate class, creating post type Notes
  */
- 
+
 global $note;
 $note = new Note();
 
@@ -86,41 +87,41 @@ $note = new Note();
  *
  */
 
-class NotesPage { 
-	
+class NotesPage {
+
 	public function wp_enqueue_styles() {
 		wp_register_style( 'notes', plugins_url( 'assets/css/custom.css', __FILE__ ), array(), filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/custom.css' ) );
 		wp_enqueue_style( 'notes');
 	}
-	
+
 	public function __construct() {
 		// Enqueue styles for the admin
 		add_action( 'admin_enqueue_scripts', array( $this, 'wp_enqueue_styles' ), 20 );
 		add_action( 'admin_menu', array($this,'notes_admin_page_menu') );
 	}
-	
+
 	public function notes_admin_page_menu() {
 		add_menu_page( 'notes', 'Notes', 'edit_posts', 'notes', array($this,'notes_admin_page'), 'dashicons-format-aside', 6 ) ;
 	}
-	
+
 	function renderForm(){
 		?>
 		<form method="POST" action="" id="notes-form">
 			<label for="post_content"><h2><?php echo _e('Add your note','Notes')?></h2></label>
-			<textarea name="post_content" id="post_content" required/></textarea>	
+			<textarea name="post_content" id="post_content" required/></textarea>
 			<input type="text" value="1" name="new_post" hidden />
-			<input type="submit" value="<?php echo _e('Add note','Notes');?>" />		
+			<input type="submit" value="<?php echo _e('Add note','Notes');?>" />
 		</form>
 		<?php
 	}
-	
+
 	function insertNote(){
 		//insterts a new note, with the info submited on the form.
 		if(isset($_POST['new_post']) == '1'){
-			
+
 			$user_id = get_current_user_id();
 			$post_content = $_POST['post_content'];
-			
+
 			// Create post object
 			$my_post = array(
 			  'post_title'    => 'note',
@@ -128,30 +129,30 @@ class NotesPage {
 			  'post_type'	  => 'note',
 			  'post_author'   => $user_id,
 			  'post_status'   => 'publish',
-		
+
 			);
-			 
+
 			// Insert the post into the database
 			wp_insert_post($my_post);
 		}
 	}
-	
-	function showNotes(){ 
-		// get notes		
+
+	function showNotes(){
+		// get notes
 		$args = array(
 		  'post_type'      => 'note',
 		  'numberposts'   => -1, //shows all the notes
 		);
-		 
+
 		$notes = get_posts( $args ); ?>
-		
+
 		<h2><?php _e('Notes submited:','Notes') ?></h2>
 		<ul><?php
-			//prin the list of notes, if there are some		
+			//prin the list of notes, if there are some
 			if ( $notes ) {
-		        foreach ( $notes as $note ) : 
-					setup_postdata( $note ); 
-					$authorId = $note->post_author;  
+		        foreach ( $notes as $note ) :
+					setup_postdata( $note );
+					$authorId = $note->post_author;
 					$author = get_user_by( 'id', $authorId )->display_name;
 					$url = get_bloginfo('url');
 					$noteContent = esc_html($note->post_content)
@@ -165,20 +166,20 @@ class NotesPage {
 		        endforeach;
 		        wp_reset_postdata();
 		    }
-		    else { 
+		    else {
 			    //in case there are no notes yet, It will show a message
 			    echo _e('No notes submited yet','Notes');
 		    } ?>
-	    </ul><?php 
+	    </ul><?php
 	}
-	
-	function notes_admin_page() { 
+
+	function notes_admin_page() {
 		//creating the admin page
-		?> 
+		?>
 		<section id="notes-plugin">
 			<h1 class="wp-heading-inline"><?php  _e('Notes','Notes') ?></h1>
 			<div class="wrapper">
-				<div class="span7">		
+				<div class="span7">
 					<?php $this->renderForm(); ?>
 					<?php $this->insertNote(); ?>
 				</div>
@@ -187,7 +188,7 @@ class NotesPage {
 				</div>
 			</div>
 		</section>
-	<?php 
+	<?php
 	}
 
 }
@@ -196,7 +197,7 @@ class NotesPage {
 /**
  * Instantiate class, creating admin page
  */
- 
+
 global $NotesPage;
 $notesPage = new NotesPage();
 
